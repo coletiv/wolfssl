213a2061c3e97c836159363fec7c409e152a01eb
Luis Laugga <luis@laugga.com>
2016-02-14 23:33:11 +0100

    Custom user settings with specific defines for cipher suites.

--- IDE/iOS/user_settings.h
+++ IDE/iOS/user_settings.h
@@ -1,9 +1,22 @@
 /* Configuration */
 #define IPHONE	/* Needed for Xcode */
+#define OPENSSL_EXTRA
 #define HAVE_HASHDRBG
 #define HAVE_AESGCM
 #define WOLFSSL_SHA512
 #define WOLFSSL_SHA384
+#define DEBUG_WOLFSSL
+//#define NO_WOLFSSL_SERVER
+#define SINGLE_THREADED
+
+#define HAVE_ECC
+#define WOLFSSL_STATIC_RSA
+
+//#define NO_FILESYSTEM
+#define USE_CERT_BUFFERS_1024
+#define USE_CERT_BUFFERS_2048
+
+#define DEBUG_WOLFSSL
 
 #ifdef HAVE_FIPS
 #define NO_MD4
